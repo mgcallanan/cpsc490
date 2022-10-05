b8794ce3acc5d1c11cb6e5a962923c9aba499753
mgcallanan <marygcallanan@gmail.com>
2022-10-05 10:59:55 -0400

    add ts syntax formatting for vscode

--- polygon-draw/sketch.js
+++ polygon-draw/sketch.js
@@ -1,92 +1,33 @@
-// Two ArrayLists to store the vertices for two shapes
-// This example assumes that each shape will have the same
-// number of vertices, i.e. the size of each ArrayList will be the same
-let circle = [];
-let square = [];
+/// <reference path="../TSDef/p5.global-mode.d.ts" />
 
-// An ArrayList for a third set of vertices, the ones we will be drawing
-// in the window
-let morph = [];
-
-// This boolean variable will control if we are morphing to a circle or square
-let state = false;
+let lines = [];
+let firstClicked = false;
+let secondClicked = false;
+let numLines = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-
-  // Create a circle using vectors pointing from center
-  for (let angle = 0; angle < 360; angle += 9) {
-    // Note we are not starting from 0 in order to match the
-    // path of a circle.
-    let v = p5.Vector.fromAngle(radians(angle - 135));
-    v.mult(100);
-    circle.push(v);
-    // Let's fill out morph ArrayList with blank PVectors while we are at it
-    morph.push(createVector());
-  }
-
-  // A square is a bunch of vertices along straight lines
-  // Top of square
-  for (let x = -50; x < 50; x += 10) {
-    square.push(createVector(x, -50));
-  }
-  // Right side
-  for (let y = -50; y < 50; y += 10) {
-    square.push(createVector(50, y));
-  }
-  // Bottom
-  for (let x = 50; x > -50; x -= 10) {
-    square.push(createVector(x, 50));
-  }
-  // Left side
-  for (let y = 50; y > -50; y -= 10) {
-    square.push(createVector(-50, y));
-  }
 }
 
 function draw() {
   background(0);
 
-  // We will keep how far the vertices are from their target
-  let totalDistance = 0;
-
-  // Look at each vertex
-  for (let i = 0; i < circle.length; i++) {
-    let v1;
-    // Are we lerping to the circle or square?
-    if (state) {
-      v1 = circle[i];
-    } else {
-      v1 = square[i];
-    }
-    // Get the vertex we will draw
-    let v2 = morph[i];
-    // Lerp to the target
-    v2.lerp(v1, 0.1);
-    // Check how far we are from target
-    totalDistance += p5.Vector.dist(v1, v2);
+  for (i = 0; i < lines.length; i++) {
+    line(lines[i][0], lines[i][1], lines[i][2], lines[i][3]);
   }
-
-  // If all the vertices are close, switch shape
-  if (totalDistance < 0.1) {
-    state = !state;
-  }
-
-  // Draw relative to center
-  translate(width / 2, height / 2);
-  strokeWeight(4);
-  // Draw a polygon that makes up all the vertices
-  beginShape();
-  noFill();
-  stroke(255);
-
-  morph.forEach((v) => {
-    vertex(v.x, v.y);
-  });
-  endShape(CLOSE);
 }
 
 /* full screening will change the size of the canvas */
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
+}
+
+function mouseClicked() {
+  if (!firstClicked && !secondClicked) {
+    firstClicked = true;
+    lines.append([mouseX, mouseY]);
+  } else if (firstClicked && !secondClicked) {
+    secondClicked = true;
+    numLines++;
+  }
 }
